fd1ccaf81efe4d358394dff12b10fa1622776435
SiroProg <070777sss@gmail.com>
2023-08-10 18:34:07 +0500

    Carts decode

--- json_decode.dart
+++ json_decode.dart
@@ -3,73 +3,145 @@ import 'dart:convert';
 void main(List<String> args) {
   String response = '''
 {
-    "products": [
+    "carts": [
         {
             "id": 1,
-            "title": "iPhone 9",
-            "description": "An apple mobile which is nothing like apple",
-            "price": 549,
-            "discountPercentage": 12.96,
-            "rating": 4.69,
-            "stock": 94,
-            "brand": "Apple",
-            "category": "smartphones",
-            "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-            "images": [
-                "https://i.dummyjson.com/data/products/1/1.jpg",
-                "https://i.dummyjson.com/data/products/1/2.jpg",
-                "https://i.dummyjson.com/data/products/1/3.jpg",
-                "https://i.dummyjson.com/data/products/1/4.jpg",
-                "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
-            ]
+            "products": [
+                {
+                    "id": 59,
+                    "title": "Spring and summershoes",
+                    "price": 20,
+                    "quantity": 3,
+                    "total": 60,
+                    "discountPercentage": 8.71,
+                    "discountedPrice": 55
+                },
+                {
+                    "id": 88,
+                    "title": "TC Reusable Silicone Magic Washing Gloves",
+                    "price": 29,
+                    "quantity": 2,
+                    "total": 58,
+                    "discountPercentage": 3.19,
+                    "discountedPrice": 56
+                },
+                {
+                    "id": 18,
+                    "title": "Oil Free Moisturizer 100ml",
+                    "price": 40,
+                    "quantity": 2,
+                    "total": 80,
+                    "discountPercentage": 13.1,
+                    "discountedPrice": 70
+                },
+                {
+                    "id": 95,
+                    "title": "Wholesale cargo lashing Belt",
+                    "price": 930,
+                    "quantity": 1,
+                    "total": 930,
+                    "discountPercentage": 17.67,
+                    "discountedPrice": 766
+                },
+                {
+                    "id": 39,
+                    "title": "Women Sweaters Wool",
+                    "price": 600,
+                    "quantity": 2,
+                    "total": 1200,
+                    "discountPercentage": 17.2,
+                    "discountedPrice": 994
+                }
+            ],
+            "total": 2328,
+            "discountedTotal": 1941,
+            "userId": 97,
+            "totalProducts": 5,
+            "totalQuantity": 10
         },
         {
             "id": 2,
-            "title": "iPhone X",
-            "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
-            "price": 899,
-            "discountPercentage": 17.94,
-            "rating": 4.44,
-            "stock": 34,
-            "brand": "Apple",
-            "category": "smartphones",
-            "thumbnail": "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
-            "images": [
-                "https://i.dummyjson.com/data/products/2/1.jpg",
-                "https://i.dummyjson.com/data/products/2/2.jpg",
-                "https://i.dummyjson.com/data/products/2/3.jpg",
-                "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
-            ]
+            "products": [
+                {
+                    "id": 96,
+                    "title": "lighting ceiling kitchen",
+                    "price": 30,
+                    "quantity": 2,
+                    "total": 60,
+                    "discountPercentage": 14.89,
+                    "discountedPrice": 51
+                },
+                {
+                    "id": 91,
+                    "title": "Black Motorbike",
+                    "price": 569,
+                    "quantity": 3,
+                    "total": 1707,
+                    "discountPercentage": 13.63,
+                    "discountedPrice": 1474
+                },
+                {
+                    "id": 9,
+                    "title": "Infinix INBOOK",
+                    "price": 1099,
+                    "quantity": 1,
+                    "total": 1099,
+                    "discountPercentage": 11.83,
+                    "discountedPrice": 969
+                },
+                {
+                    "id": 16,
+                    "title": "Hyaluronic Acid Serum",
+                    "price": 19,
+                    "quantity": 1,
+                    "total": 19,
+                    "discountPercentage": 13.31,
+                    "discountedPrice": 16
+                },
+                {
+                    "id": 54,
+                    "title": "Pubg Printed Graphic T-Shirt",
+                    "price": 46,
+                    "quantity": 3,
+                    "total": 138,
+                    "discountPercentage": 16.44,
+                    "discountedPrice": 115
+                }
+            ],
+            "total": 3023,
+            "discountedTotal": 2625,
+            "userId": 30,
+            "totalProducts": 5,
+            "totalQuantity": 10
         }
     ],
-    "total": 100,
+    "total": 20,
     "skip": 0,
     "limit": 2
 }
 ''';
 
-  ProductModel productModel = ProductModel.fromJson(jsonDecode(response));
-
-  print(productModel.products![0].description);
+  Carts carts = Carts.fromJson(jsonDecode(response));
+  print(carts.carts[0].products[0].id);
 }
 
-class ProductModel {
-  List<Product>? products;
+class Carts {
+  List<Cart> carts;
   int? total;
   int? skip;
   int? limit;
 
-  ProductModel({
-    this.products,
+  Carts({
+    required this.carts,
     this.total,
     this.skip,
     this.limit,
   });
 
-  factory ProductModel.fromJson(Map<String, Object?> json) {
-    return ProductModel(
-      products:
-          (json['products'] as List?)?.map((e) => Product.fromJson(e)).toList(),
+  factory Carts.fromJson(Map<String, Object?> json) {
+    return Carts(
+      carts:
+          (json['carts'] as List?)?.map((e) => Cart.fromJson(e)).toList() ?? [],
       total: json['total'] as int?,
       skip: json['skip'] as int?,
       limit: json['limit'] as int?,
@@ -77,47 +149,69 @@ class ProductModel {
   }
 }
 
+class Cart {
+  int? id;
+  List<Product> products;
+  int? total;
+  int? discountedTotal;
+  int? userId;
+  int? totalProducts;
+  int? totalQuantity;
+
+  Cart({
+    this.id,
+    required this.products,
+    this.total,
+    this.discountedTotal,
+    this.userId,
+    this.totalProducts,
+    this.totalQuantity,
+  });
+
+  factory Cart.fromJson(Map<String, Object?> json) {
+    return Cart(
+      id: json['id'] as int?,
+      products: (json['products'] as List?)
+              ?.map((e) => Product.fromJson(e))
+              .toList() ??
+          [],
+      total: json['total'] as int?,
+      discountedTotal: json['discountedTotal'] as int?,
+      userId: json['userId'] as int?,
+      totalProducts: json['totalProducts'] as int?,
+      totalQuantity: json['totalQuantity'] as int?,
+    );
+  }
+}
+
 class Product {
   int? id;
   String? title;
-  String? description;
-  num? price;
+  int? price;
+  int? quantity;
+  int? total;
   double? discountPercentage;
-  double? rating;
-  int? stock;
-  String? brand;
-  String? category;
-  String? thumbnail;
-  List<String> images;
+  int? discountedPrice;
 
   Product({
     this.id,
     this.title,
-    this.description,
     this.price,
+    this.quantity,
+    this.total,
     this.discountPercentage,
-    this.rating,
-    this.stock,
-    this.brand,
-    this.category,
-    this.thumbnail,
-    required this.images,
+    this.discountedPrice,
   });
 
   factory Product.fromJson(Map<String, Object?> json) {
     return Product(
       id: json['id'] as int?,
       title: json['title'] as String?,
-      description: json['description'] as String?,
-      price: json['price'] as num?,
+      price: json['price'] as int?,
+      quantity: json['quantity'] as int?,
+      total: json['total'] as int?,
       discountPercentage: json['discountPercentage'] as double?,
-      rating: json['rating'] as double?,
-      stock: json['stock'] as int?,
-      brand: json['brand'] as String?,
-      category: json['category'] as String?,
-      thumbnail: json['thumbnail'] as String?,
-      images:
-          (json['images'] as List?)?.map((e) => e.toString()).toList() ?? [],
+      discountedPrice: json['discountedPrice'] as int?,
     );
   }
 }
